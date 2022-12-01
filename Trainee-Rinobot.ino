9dc835caacd606760219e580808f533367a3d0b2
Gabriel <gabriel06tavares29@gmail.com>
2022-12-01 20:01:34 -0300

    Criado arquivo .Ino

---
+++ Trainee-Rinobot.ino
@@ -0,0 +1,31 @@
+#include "Arduino.h"
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(RIGHT_MOTOR_PWM_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN1_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN2_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_PWM_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_IN1_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_IN2_PIN, OUTPUT);
+  pinMode(FRONT_SENSOR_PIN, INPUT);
+  pinMode(LEFT_SENSOR_PIN, INPUT);
+  pinMode(RIGHT_SENSOR_PIN, INPUT);
+  pinMode(MICRO_START_SIGNAL_PIN, INPUT);
+
+  pinMode(STDBY, OUTPUT);
+  digitalWrite(STDBY, HIGH);
+
+  ledcAttachPin(LEFT_MOTOR_PWM_PIN, LEFT_MOTOR_CHANNEL);
+  ledcSetup(LEFT_MOTOR_CHANNEL, 12000, 8);
+
+  ledcAttachPin(RIGHT_MOTOR_PWM_PIN, RIGHT_MOTOR_CHANNEL);
+  ledcSetup(RIGHT_MOTOR_CHANNEL, 12000, 8);
+}
+
+void loop()
+{
+  static Robot robot;
+  robot.update();
+}
