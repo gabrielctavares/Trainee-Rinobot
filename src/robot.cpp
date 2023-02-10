9d4e0c8da1fae345c8bff5801d5d87986e21332b
Gabriel <gabriel06tavares29@gmail.com>
2023-02-10 13:18:47 -0300

    Ajuste na lógica de inicialização

--- src/robot.cpp
+++ src/robot.cpp
@@ -32,23 +32,30 @@ void Robot::update()
     this->vision.updateEnemyPosition(this->front_sensor, this->full_left_sensor, this->full_right_sensor, this->left_sensor, this->right_sensor);
 
     if(this->ustart.state == uStartState::START){
-        this->initial_strategy = get_selected_strategy(STRATEGY_PIN_A, STRATEGY_PIN_B, STRATEGY_PIN_C);
-
-        if(this->robot_state == RobotState::INITIAL_STRATEGY){
-            this->initial_strategy->update(this->left_motor, this->right_motor);
+        switch(this->robot_state){
+            case RobotState::AWAITING_START: {
+                this->initial_strategy = get_selected_strategy(STRATEGY_PIN_A, STRATEGY_PIN_B, STRATEGY_PIN_C);
+                this->robot_state == RobotState::INITIAL_STRATEGY;
+                break;
+            }
+            case RobotState::INITIAL_STRATEGY: {
+                this->initial_strategy->update(this->left_motor, this->right_motor);
             
-            if(this->initial_strategy->strategy_finished)
-                this->robot_state == RobotState::AUTO_STRATEGY;
-        }            
-        else if (this->robot_state == RobotState::AUTO_STRATEGY)
-            this->auto_strategy.updateMotors(this->vision, this->left_motor, this->right_motor);
+                if(this->initial_strategy->strategy_finished)
+                    this->robot_state == RobotState::AUTO_STRATEGY;
+                break;
+            }
+            case RobotState::AUTO_STRATEGY: {
+                this->auto_strategy.updateMotors(this->vision, this->left_motor, this->right_motor);
+                break;
+            }
+        }                
     }
     else{
         this->robot_state == RobotState::STOPPED;
-        this->left_motor->setPower(0);
-        this->right_motor->setPower(0);
-    }
-        
+        this->left_motor.setPower(0);
+        this->right_motor.setPower(0);
+    }        
 }
 
 
