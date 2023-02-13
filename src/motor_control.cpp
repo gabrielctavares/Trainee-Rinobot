1d588be40c63ca8a64582831683e970edce8d95c
Gabriel <gabriel06tavares29@gmail.com>
2023-02-13 19:19:36 -0300

    adição do channel do motor

--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 #include "../include/motor_control.h"
 
-MotorConfig::MotorConfig(int pwm_pin, int in1_pin, int in2_pin, float speed_const)
+MotorConfig::MotorConfig(int pwm_pin, int in1_pin, int in2_pin, float speed_const, int channel)
 {
     this->pwm_pin = pwm_pin;
     this->in1_pin = in1_pin;
     this->in2_pin = in2_pin;
     this->speed_const = speed_const;
+    this->channel = channel;
 }
 
-MotorControl::MotorControl(int pwm_pin, int in1_pin, int in2_pin, float speed_const): config(pwm_pin, in1_pin, in2_pin, speed_const)
+MotorControl::MotorControl(int pwm_pin, int in1_pin, int in2_pin, float speed_const, int channel): config(pwm_pin, in1_pin, in2_pin, speed_const, channel)
 {
     
     
@@ -17,9 +18,12 @@ MotorControl::MotorControl(int pwm_pin, int in1_pin, int in2_pin, float speed_co
 
 void MotorControl::setPower(int power)
 {    
-    this->power = abs((abs(power) > abs(this->max_power)) ? this->max_power : power); 
-
+    this->power = abs(power);
+    
+    if(this->power > abs(this->max_power))
+        this->power = abs(this->max_power);
+        
     digitalWrite(this->config.in1_pin, power >= 0);
     digitalWrite(this->config.in2_pin, power < 0);
-    ledcWrite(this->config.pwm_pin, map(this->power, 0, 100, 0, 255));
+    ledcWrite(this->config.channel, map(this->power, 0, 100, 0, 255));
 }
